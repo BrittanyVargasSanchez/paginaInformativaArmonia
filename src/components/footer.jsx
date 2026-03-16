ce921f2ffbc6ad53b30ad48f709925f13a0c60ed
Brit <brittany.vargassss@gmail.com>
2026-03-16 15:02:16 -0600

    posiblemente coso final

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="footer-banner">
       <div className="text-content">
-        <img src="/src/assets/images/logo.png" alt="Logo" />
+        <img src="./src/assets/images/logo.png" alt="Logo" />
         <p>Rincón de agricultura y alimentación saludable</p>
         <p>© 2026 - Todos los derechos reservados</p>
       </div>
